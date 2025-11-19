065d0c0bd3fb556b7e17f7fb96415e865d3c0c4e
unknown <fotsofabrice3@gmail.com>
2025-11-19 23:35:07 +0100

    second commit 9

--- election_project/settings.py
+++ election_project/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 import os
 MEDIA_URL = '/media/'
