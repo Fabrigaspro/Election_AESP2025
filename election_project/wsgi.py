3d3c12155bff89448d4c079dc901c1aa44c5860f
unknown <fotsofabrice3@gmail.com>
2025-11-20 20:51:35 +0100

    com 6

--- election_project/wsgi.py
+++ election_project/wsgi.py
@@ -15,5 +15,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'election_project.settings')
 
 application = get_wsgi_application()
-
-application = WhiteNoise(application, root=settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
