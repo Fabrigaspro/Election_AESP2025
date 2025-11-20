0154bf3f1b50285cc82cc5cca781818382fa4d22
unknown <fotsofabrice3@gmail.com>
2025-11-20 18:16:19 +0100

    com 1

--- election_project/settings.py
+++ election_project/settings.py
@@ -120,8 +120,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
-USE_I18N = True
 USE_TZ = True
+USE_I18N = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
