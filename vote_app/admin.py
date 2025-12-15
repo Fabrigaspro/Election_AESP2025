b41c46ec32e07ca1c9886ae8b5d26572b5919e66
unknown <fotsofabrice3@gmail.com>
2025-12-15 21:12:12 +0100

    com 13

--- vote_app/admin.py
+++ vote_app/admin.py
@@ -11,7 +11,7 @@ admin.site.index_title = "Tableau de bord"
 @admin.register(Profile)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('matricule','get_fullname','telephone', 'role', 'status', 'campus', 'cycle', 'specialite', 'niveau', 'is_connected', 'last_activity', 'is_actually_connected', 'has_active_session', 'session_expires', 'session_token', 'has_voted', 'is_admin', 'photo', 'recu')
-    list_filter = ('role', 'status', 'cycle', 'niveau')
+    list_filter = ('matricule', 'role', 'status', 'cycle', 'niveau', 'campus')
     search_fields = ('cycle', 'niveau',)
 
     def get_fullname(self, obj):
